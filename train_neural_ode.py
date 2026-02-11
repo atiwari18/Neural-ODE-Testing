8ad1388726c37a9e654805dea7b9d3a995a6e37d
Atharva Tiwari <126433749+atiwari18@users.noreply.github.com>
2026-02-10 19:44:13 -0500

    refactored training script

--- train_neural_ode.py
+++ train_neural_ode.py
@@ -1,6 +1,6 @@
 import torch
-from models.neural_ode import ODEFunc, train_ode, plot_loss, extrapolate, plot_vector_field, plot_extrapolation, plot_learned_dynamics_vs_true, plot_spiral_extrapolation
-from dataset.data import generate_irregular, generate_spiral
+from models.neural_ode import ODEFunc, train_ode, plot_loss, extrapolate, plot_comparison, plot_extrapolation, plot_learned_dynamics_vs_true, plot_spiral_extrapolation
+from dataset.data import SineDynamics, generate_sine
 
 # Device setup (GPU if available, else CPU)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -8,45 +8,30 @@ print(f"Using device: {device}")
 
 # Load Data
 print("Loading Data...")
-#t_train, state_train = generate_irregular(100)
-data = generate_spiral(500, t_max=10)
-y0 = torch.tensor([1.0, 0.0])
-t = torch.linspace(0, 10, 100)
-
-# Sort by time and keep state aligned
-# sorted_indices = torch.argsort(t_train)
-# t_train = t_train[sorted_indices]
-# state_train = state_train[sorted_indices]
-
-# # Move data to device
-# t_train = t_train.to(device)
-# state_train = state_train.to(device)
-data = data.to(device)
-y0 = y0.to(device)
+true_func = SineDynamics()
+t, y0, true_traj = generate_sine(true_func, batch_size=16, n_samples=100, t_max=4*torch.pi, device=device)
 t = t.to(device)
-
+y0 = y0.to(device)
+true_traj = true_traj.to(device)
 print("Data Loaded!")
-print(f"State shape: y{y0.shape}")  # Should be [100, 2]
+print(f"State shape: y{y0.shape}") 
 
 # Create model, optimizer and criterion
 model = ODEFunc(hidden_dim=64).to(device)
-optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 criterion = torch.nn.MSELoss()
 
 # Training
 print("\nTraining Neural ODE...")
-losses = train_ode(model, 250, optimizer, criterion, t, data)
+losses =  train_ode(model, 100, optimizer, criterion, true_traj=true_traj, t=t, y0=y0)
 
 # Plotting
 print("\nGenerating plots...")
-plot_loss(losses, file_name="Losses (spiral-time-conditioned-500).png")
+plot_loss(losses, file_name="Losses (multiple-traj-100).png")
+plot_comparison(true_func, model, device=device, file_name="spiral_comparision (multiple-traj-100).png")
 
-t_future, state_future = extrapolate(model, t, data, device, t_max=20)
-
-#plot_vector_field(model, file_name="Learned Vector Field (spiral-dataset).png", device=device)
-
-#plot_extrapolation(t, data, t_future, state_future, "Extrapolation for Sine Wave (spiral-dataset).png", model, device)
-
-#plot_learned_dynamics_vs_true(model, device=device, file_name="Learned Dynamics (spiral-dataset).png")
-
-plot_spiral_extrapolation(t, data, state_future, file_name="Spiral Extrapolation (spiral-time-conditioned-500).png")
+#3xtrapolating one
+single_y0 = y0[0:1]
+single_true = true_traj[:, 0:1, :]
+t_future, state_future = extrapolate(model, t, single_true[:, 0, :], device=device, t_max=30)
+plot_spiral_extrapolation(t, single_true[:, 0, :], state_future, true_func=true_func, file_name="single_extrapolation (multiple-traj-100).png", model=model, device=device)
